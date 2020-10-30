a596178d41b4163db569cb83aaf90ae9156671ab
bajus1 <73716118+bajus1@users.noreply.github.com>
2020-10-30 18:35:21 +0000

    Update assignment.sh

--- assignment.sh
+++ assignment.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>log.out 2>&1
+
+# Everything below will go to the file 'log.out':
+
+
 echo "Which action would you like to perform?"
 
 select action in Make Add Quit
