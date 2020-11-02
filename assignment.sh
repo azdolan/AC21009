a7e902c19a46c56ab1a6e147d59256d1f18dddb1
azdolan <55203039+azdolan@users.noreply.github.com>
2020-11-02 10:55:12 +0000

    Add files via upload
    I've managed to do something which allows the user to create a new file in the directory, I think it's a bit convoluted and could be tidied up somehow but it works for now. I also found out that we don't need to ask the user for the file extension because the script will automatically assume it's a txt file when you use "touch" to create a new file. I created two functions and called them in the menu to tidy up the code

--- assignment.sh
+++ assignment.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
 
-exec 3>&1 4>&2
-trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>log.out 2>&1
+#a function to create a repository
+makeRepository () {
+	echo "What is the name of the directory?"
+	read -p "Enter directory name: " dirname
+	mkdir $dirname
+}
 
-# Everything below will go to the file 'log.out':
+#a function to add files to the repositorty
+addingFiles () {
+	echo "What is the name of the repository to add the file to?" 
+	read -p "Enter repository name: " dirname
+	cd $dirname
+	echo "What is the name of the file?"
+	read -p "File name: " filename
+	touch $filename
+}
+
 
 
 echo "Which action would you like to perform?"
-
 select action in Make Add Quit
 do
 	case $action in
 		Make)
-		echo "What is the name of the new directory?"
-		read -p "Enter directory name: " dirname
-		mkdir $dirname
-		cd $dirname
-		break
-		;;
+			makeRepository
+			break
+			;;
 		Add)
-echo "What is the name of your file and it's extension, eg page.html?"
-read -p "File name plus extension " filename
-touch $filename | $dirname
-break
-;;
-
+			addingFiles
+			break
+			;;
 		Quit)
 			break
 			;;
