19dca4d02e7587ff2b2c2172f59c78b93adac5d5
bajus1 <73716118+bajus1@users.noreply.github.com>
2020-11-09 11:09:50 +0000

    Update assignment.sh

--- assignment.sh
+++ assignment.sh
@@ -4,9 +4,18 @@
 makeRepository () {
 	echo "What is the name of the directory?"
 	read -p "Enter directory name: " dirname
+	
+	if [[ ! -d $dirname ]]; then
+	echo "error: directory does not exist"
+	exit 1
+	
+	else
+	echo "directory selected: $dirname"
 	mkdir $dirname
 	cd $dirname
-	echo "dirctory created" | tee -a out.log
+	echo "dirctory created" | tee -a out.log	
+	fi
+	
 	
 }
 
@@ -14,29 +23,65 @@ makeRepository () {
 addingFiles () {
 	echo "What is the name of the file?"
 	read -p "File name: " filename
+	
+	if [[ ! -f "$filename" ]]; then
+
+	echo "error: file $filename does not exist"
+	exit 1
+
+	
+	else
+	
 	touch $filename.txt
-	echo "file created" | tee -a out.log
+	echo "file: &filename.txt created" | tee -a out.log
+	
+	fi
 }
 
 checkingFile () {
 	ls
 	echo "What is the name of the file you want to check?"
 	read -p "Enter file name: " filename
+	if [[ ! -f "$filename" ]]; then
+
+	echo "error: file does not exist"
+	exit 1
+
+	else
 	cat $filename.txt
 	echo "file checked" | tee -a out.log
+	
+	fi
 }
 
 editingFile () {
 	ls
 	echo "What is the name of the file you want to edit: " 
 	read -p "Enter file name " filename
+	if [[ ! -f "$filename" ]]; then
+
+	echo "error: file does not exist"
+	exit 1
+
+	else
 	nano $filename.txt
 	echo "file edited" | tee -a out.log
+	
+	fi
 }
 
 creatingZip (){
 	echo "What is the name of the repository you want to Zip?"
 	read -p "Enter directory name: " dirname
+	if [[ ! -d $dirname ]]; then
+	echo "error: directory does not exist"
+	exit 1
+	else
+	echo "directory selected: $dirname "
+	echo "creating zip of : $dirname "
+		
+	
+	
 	zip -r $dirname.zip $dirname
 }
 
